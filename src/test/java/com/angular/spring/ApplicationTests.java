90f5bec5180f9e469643643f5db5ba745690ddcb
Debonnaire <debonnairengatchui@gmail.com>
2023-03-05 19:35:42 -0500

    first commit

---
+++ src/test/java/com/angular/spring/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.angular.spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
